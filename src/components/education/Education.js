0a434b736f7439ba67dac6e260fa5ee1af37c0fd
Zachary Wikel <zwikel@rams.colostate.edu>
2020-11-28 21:30:51 -0700

    Edited education

--- src/components/education/Education.js
+++ src/components/education/Education.js
@@ -1,21 +1,29 @@
-import React from "react";
+import React, {Component} from "react";
 import {Card, Jumbotron} from "react-bootstrap";
 import Tilt from "react-tilt";
 import CSU from "../../assets/education/CSU-Ram-357-Gold.png";
 import "./Education.css"
 
-const Education = () => {
-    return (
+export default class Education  extends Component{
+    render () {
+        return(
         <div id="education" className="app-nav">
             <h1>Where I Started</h1>
+            {this.renderEducationCard()}
+        </div>
+        );
+    }
+
+    renderEducationCard(){
+        return (
             <Jumbotron className="app-jumbo">
                 <Tilt options={{max: 20}}>
                     <Card className="app-card">
                         <Card.Header>
-                            <Card.Img className="education-image" src={CSU} alt="Colorado State University"/>
+                            <Card.Img className="education-image" src={CSU}/>
                         </Card.Header>
                         <Card.Body>
-                            <Card.Title className="education-card-title">Colorado State University</Card.Title>
+                            <Card.Title className="education-card-title">"Colorado State University"</Card.Title>
                             <Card.Text className="education-card-text">
                                 <strong>Bachelor of Science</strong>
                                 <br/>
@@ -30,28 +38,6 @@ const Education = () => {
                     </Card>
                 </Tilt>
             </Jumbotron>
-            {/*<Jumbotron className="app-jumbo">*/}
-            {/*    <Tilt options={{max: 20}}>*/}
-            {/*        <Card className="education-card">*/}
-            {/*            <Card.Header>*/}
-            {/*                <Card.Img className="education-image" src={COCHISE} alt="Cochise College"/>*/}
-            {/*            </Card.Header>*/}
-            {/*            <Card.Body>*/}
-            {/*                <Card.Title className="education-card-title">Cochise College</Card.Title>*/}
-            {/*                <Card.Text className="education-card-text">*/}
-            {/*                    <strong>Associate of Applied Science</strong>*/}
-            {/*                    <br/>*/}
-            {/*                    <strong>Major:</strong> Unmanned Aircraft Systems and Operations*/}
-            {/*                    <br/>*/}
-            {/*                    <strong>Graduation:</strong> Jan 2021*/}
-            {/*                    <br/>*/}
-            {/*                </Card.Text>*/}
-            {/*            </Card.Body>*/}
-            {/*        </Card>*/}
-            {/*    </Tilt>*/}
-            {/*</Jumbotron>*/}
-        </div>
-    );
-};
-
-export default Education;
+        );
+    }
+}
